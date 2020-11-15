0f6f0daff54444e46eb99993df6227d4f6b84410
hjalmar <hjalmar.thunberg@gmail.com>
2020-11-15 19:58:18 +0100

    Loaded vue2 maps inside main

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyACwSQoJfqy-dJ5rbYmiKOdqYFu8-k8S0Y' // TODO: put in env file
+  }
+})
 
 new Vue({
   render: h => h(App),
