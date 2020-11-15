b7636c133dfc8cd8087f21ea5823025a2e36fa87
hjalmar <hjalmar.thunberg@gmail.com>
2020-11-15 19:58:31 +0100

    Correctly used map inside of app view

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,25 +1,18 @@
 <template>
   <div id="app">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <map ref="map"/>
+    <Map ref="Map"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import Vue from 'vue'
-import map from './components/map.vue'
-import * as VueGoogleMaps from 'vue2-google-maps'
-
-Vue.use(VueGoogleMaps, {
-    key: 'AIzaSyACwSQoJfqy-dJ5rbYmiKOdqYFu8-k8S0Y' // TODO: put in env file
-})
-
+import Map from './components/map.vue'
 
 export default {
   name: 'App',
   components: {
-    map,
+    Map,
     HelloWorld
   }
 }
