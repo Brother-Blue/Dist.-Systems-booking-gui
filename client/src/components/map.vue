b88d181496489d815e88b55226682d3ed27669f0
hjalmar <hjalmar.thunberg@gmail.com>
2020-11-16 14:22:44 +0100

    Added functions to allow populating map + info win

--- client/src/components/map.vue
+++ client/src/components/map.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+    <!-- Google map layout -->
     <GmapMap
     ref="mapRef"
     :center="centerLocation"
@@ -11,8 +12,28 @@
         streetViewControl: false,
         rorationControl: false,
         minZoom: 5,
-        styles: mapStyles}"
-    >
+        styles: mapStyles}">
+
+        <!-- Dentist marker -->
+        <GmapMarker
+        v-for="(r, index) in offices"
+        :key="index"
+        :position="{
+        lat: r.coordinate.latitude,
+        lng: r.coordinate.longitude}"
+        :clickable="true"
+        :draggable="false"
+        @click="toggleInfoWindow(r,index)"
+        :icon="dentistStyles"/>
+
+        <!-- Dentist info window -->
+        <GmapInfoWindow
+        :options="infoOptions"
+        :position="infoWindowPos"
+        :opened="infoWinOpen"
+        @closeclick="infoWinOpen = false">
+        </GmapInfoWindow>
+
     </GmapMap>
 </div>
 </template>
@@ -26,236 +47,340 @@ export default {
                 lat: 57.708870,         
                 lng: 11.974560
             },
+            infoWindowPos: null,
+            infoWinOpen: false,
+            currentMidx: null,
+            infoOptions: {
+                content: '',
+                pixelOffset: {
+                    width: 0,
+                    height: -35
+                }
+            },
+            dentistStyles: {
+                url: require('../assets/dental-location.svg'),
+                scaledSize: {
+                    width: 500,
+                    height: 500,
+                    f: 'px',
+                    b: 'px'
+                }
+            },
+            offices: [
+                {
+                    id: 1,
+                    name: "Your Dentist",
+                    owner: "Dan Tist",
+                    dentists: 1,
+                    address: "Spannmålsgatan 20",
+                    city: "Gothenburg",
+                    coordinate: {
+                        latitude: 11.969388,
+                        longitude: 57.707619
+                    },
+                    openinghours: {
+                        monday: "9:00-17:00",
+                        tuesday: "8:00-17:00",
+                        wednesday: "7:00-16:00",
+                        thursday: "9:00-17:00",
+                        friday: "9:00-15:00"
+                    }
+                },
+                {
+                    id: 2,
+                    name: "Tooth Fairy Dentist",
+                    owner: "Tooth Fairy",
+                    dentists: 1,
+                    address: "Slottskogen",
+                    city: "Gothenburg",
+                    coordinate: {
+                        latitude: 11.942625,
+                        longitude: 57.685255
+                    },
+                    openinghours: {
+                        monday: "7:00-19:00",
+                        tuesday: "7:00-19:00",
+                        wednesday: "7:00-19:00",
+                        thursday: "7:00-19:00",
+                        friday: "7:00-19:00"
+                    }
+                },
+                {
+                    id: 3,
+                    name: "The Crown",
+                    owner: "Carmen Corona",
+                    dentists: 1,
+                    address: "Lindholmsallén 19",
+                    city: "Gothenburg",
+                    coordinate: {
+                        latitude: 11.940386,
+                        longitude: 57.709872
+                    },
+                    openinghours: {
+                        monday: "6:00-15:00",
+                        tuesday: "8:00-17:00",
+                        wednesday: "7:00-12:00",
+                        thursday: "7:00-17:00",
+                        friday: "8:00-16:00"
+                    },
+                }
+            ],
             mapStyles: [
-        {
-          elementType: 'geometry',
-          stylers: [
+                {
+                    elementType: 'geometry',
+                    stylers: [
+                        {
+                            color: '#242f3e'
+                        }
+                    ]
+                },
+                {   
+                    elementType: 'labels',
+                    stylers: [
+                        {
+                            visibility: 'off'
+                        }
+                    ]
+                },
             {
-              color: '#242f3e'
-            }
-          ]
-        },
-        {
-          elementType: 'labels',
-          stylers: [
+                elementType: 'labels.text.fill',
+                stylers: [
+                    {
+                        color: '#746855'
+                    }
+                ]
+            },
             {
-              visibility: 'off'
-            }
-          ]
-        },
-        {
-          elementType: 'labels.text.fill',
-          stylers: [
+                elementType: 'labels.text.stroke',
+                stylers: [
+                    {
+                        color: '#242f3e'
+                    }
+                ]
+            },
             {
-              color: '#746855'
-            }
-          ]
-        },
-        {
-          elementType: 'labels.text.stroke',
-          stylers: [
+                featureType: 'administrative.country',
+                stylers: [
+                    {
+                        visibility: 'on'
+                    }
+                ]
+            },
             {
-              color: '#242f3e'
-            }
-          ]
-        },
-        {
-          featureType: 'administrative.country',
-          stylers: [
+                featureType: 'administrative.land_parcel',
+                stylers: [
+                    {
+                        visibility: 'off'
+                    }
+                ]
+            },
             {
-              visibility: 'on'
-            }
-          ]
-        },
-        {
-          featureType: 'administrative.land_parcel',
-          stylers: [
+                featureType: 'administrative.locality',
+                stylers: [
+                    {
+                        visibility: 'on'
+                    }
+                ]
+            },
             {
-              visibility: 'off'
-            }
-          ]
-        },
-        {
-          featureType: 'administrative.locality',
-          stylers: [
+                featureType: 'administrative.locality',
+                elementType: 'labels.text.fill',
+                stylers: [
+                    {
+                        color: '#d59563'
+                    }
+                ]
+            },
             {
-              visibility: 'on'
-            }
-          ]
-        },
-        {
-          featureType: 'administrative.locality',
-          elementType: 'labels.text.fill',
-          stylers: [
+                featureType: 'administrative.neighborhood',
+                stylers: [
+                    {
+                        visibility: 'on'
+                    }
+                ]
+            },
             {
-              color: '#d59563'
-            }
-          ]
-        },
-        {
-          featureType: 'administrative.neighborhood',
-          stylers: [
+                featureType: 'administrative.province',
+                stylers: [
+                    {
+                        visibility: 'on'
+                    }
+                ]
+            },
             {
-              visibility: 'on'
-            }
-          ]
-        },
-        {
-          featureType: 'administrative.province',
-          stylers: [
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [
+                    {
+                        color: '#d59563'
+                    }
+                ]
+            },
             {
-              visibility: 'on'
-            }
-          ]
-        },
-        {
-          featureType: 'poi',
-          elementType: 'labels.text.fill',
-          stylers: [
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [
+                    {
+                        color: '#263c3f'
+                    }
+                ]
+            },
             {
-              color: '#d59563'
-            }
-          ]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'geometry',
-          stylers: [
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [
+                    {
+                        color: '#6b9a76'
+                    }
+                ]
+            },
             {
-              color: '#263c3f'
-            }
-          ]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'labels.text.fill',
-          stylers: [
+                featureType: 'road',
+                stylers: [
+                    {
+                        visibility: 'on'
+                    }
+                ]
+            },
             {
-              color: '#6b9a76'
-            }
-          ]
-        },
-        {
-          featureType: 'road',
-          stylers: [
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [
+                    {
+                        color: '#38414e'
+                    }
+                ]
+            },
             {
-              visibility: 'on'
-            }
-          ]
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry',
-          stylers: [
+                featureType: 'road',
+                elementType: 'geometry.stroke',
+                stylers: [
+                    {
+                        color: '#212a37'
+                }
+            ]
+            },
             {
-              color: '#38414e'
-            }
-          ]
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry.stroke',
-          stylers: [
+                featureType: 'road',
+                elementType: 'labels.text.fill',
+                stylers: [
+                    {
+                        color: '#9ca5b3'
+                }
+            ]
+            },
             {
-              color: '#212a37'
-            }
-          ]
-        },
-        {
-          featureType: 'road',
-          elementType: 'labels.text.fill',
-          stylers: [
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [
+                    {
+                        color: '#746855'
+                    }
+                ]
+            },
             {
-              color: '#9ca5b3'
-            }
-          ]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry',
-          stylers: [
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [
+                    {
+                        color: '#1f2835'
+                    }
+                ]
+            },
             {
-              color: '#746855'
-            }
-          ]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry.stroke',
-          stylers: [
+                featureType: 'road.highway',
+                elementType: 'labels.text.fill',
+                stylers: [
+                    {
+                        color: '#f3d19c'
+                    }
+                ]
+            },
             {
-              color: '#1f2835'
-            }
-          ]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'labels.text.fill',
-          stylers: [
+                featureType: 'transit',
+                stylers: [
+                    {
+                        visibility: 'on'
+                    }
+                ]
+            },
             {
-              color: '#f3d19c'
-            }
-          ]
-        },
-        {
-          featureType: 'transit',
-          stylers: [
+                featureType: 'transit',
+                elementType: 'geometry',
+                stylers: [
+                    {
+                        color: '#2f3948'
+                    }
+                ]
+            },
             {
-              visibility: 'on'
-            }
-          ]
-        },
-        {
-          featureType: 'transit',
-          elementType: 'geometry',
-          stylers: [
+                featureType: 'transit.station',
+                elementType: 'labels.text.fill',
+                stylers: [
+                    {
+                        color: '#d59563'
+                    }
+                ]
+            },
             {
-              color: '#2f3948'
-            }
-          ]
-        },
-        {
-          featureType: 'transit.station',
-          elementType: 'labels.text.fill',
-          stylers: [
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [
+                    {
+                        color: '#17263c'
+                    }
+                ]
+            },
             {
-              color: '#d59563'
-            }
-          ]
-        },
-        {
-          featureType: 'water',
-          elementType: 'geometry',
-          stylers: [
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [
+                    {
+                        color: '#515c6d'
+                    }
+                ]
+            },
             {
-              color: '#17263c'
+                featureType: 'water',
+                elementType: 'labels.text.stroke',
+                stylers: [
+                    {
+                        color: '#17263c'
+                    }
+                ]
             }
-          ]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.fill',
-          stylers: [
-            {
-              color: '#515c6d'
-            }
-          ]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.stroke',
-          stylers: [
-            {
-              color: '#17263c'
-            }
-          ]
+        ]
         }
-      ]
-        }
+    },
+    methods: {
+            // Show info window based on which bar pressed
+    toggleInfoWindow: function (offices, idx) {
+      this.infoWindowPos = {
+        lat: offices.coordinate.latitude,
+        lng: offices.coordinate.longitude
+      }
+      this.infoOptions.content = this.getInfoWindowContent(offices)
+
+      // If same bar is clicked, close window. Else open
+      if (this.currentMidx === idx) {
+        this.infoWinOpen = !this.infoWinOpen
+      } else {
+        this.infoWinOpen = true
+        this.currentMidx = idx
+      }
+    },
+    getInfoWindowContent: function (offices) {
+      return (`  <div>
+  <div>
+    <p></p>
+    <h6><b>${offices.name}</b></h6>
+    <p>${offices.address}</p>
+  </div>
+</div>`)
+    }
     }
 }
 </script>
 
 <style scoped>
-
 </style>
