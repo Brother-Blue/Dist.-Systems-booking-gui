5fbd4ff354b2c2f9b1d0b2fb0aac1d5e2814dea0
jensenb <jensenb@student.chalmers.se>
2020-11-16 16:33:55 +0100

    Add new icon to map

--- client/src/components/map.vue
+++ client/src/components/map.vue
@@ -57,7 +57,7 @@ export default {
                 }
             },
             dentistStyles: {
-                url: require('../assets/dental-location.svg'),
+                url: require('../assets/dentist-icon.svg'),
                 scaledSize: {
                     width: 50,
                     height: 50,
