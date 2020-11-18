6550a7ca01580108b49565a3f27278e6f6b7e571
jensenb <jensenb@student.chalmers.se>
2020-11-18 20:00:18 +0100

    Add bootstrap css

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,8 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 import VueGoodTablePlugin from 'vue-good-table'
 import 'vue-good-table/dist/vue-good-table.css'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 
 Vue.use(BootstrapVue)
