242bcd382e2b67f8f11f7188e30271b5a2ed87a2
jensenb <jensenb@student.chalmers.se>
2020-11-18 13:45:02 +0100

    Resolve all issues with non-functioning router

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import App from './App.vue'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
@@ -11,11 +10,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'app',
-      component: App
-    },
-    {
-      path: '/home',
       name: 'home',
       component: Home
     }
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="home">
     <div id="map-div">
-        <h1> Dentisimo THIS IS NO LONGER THE MAIN PAGE; IT CREATED ERRORS </h1>
+        <h1> Dentisimo</h1>
         <h4>Your <span id="available"> available </span> local dentist office</h4>
         <p id="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <Map ref="Map"
