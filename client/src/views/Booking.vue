a5880c3e5d49c21efee1bcd6b74a5cb8f1e9c27b
ilinus <ilinus@student.chalmers.se>
2020-12-02 16:14:00 +0100

    Added functions to get time slots for each dentist office depending on openinghours. They are then represented on each offices bokkingview  a buttons as of now.

--- client/src/views/Booking.vue
+++ client/src/views/Booking.vue
@@ -6,10 +6,37 @@
         id="table"
         :office="office"/>
     </div>
-    <div id="table-div">
-        <TimeBooker
-        id="table"
-        :office="office"/>
+    <div>
+      <b-row>
+      <h1>Monday</h1>
+      <b-col v-for="mondayslot in mondayslots" v-bind:key="mondayslot" align-h="center">
+        <b-button close>{{ mondayslot }}</b-button>
+      </b-col>
+      </b-row>
+      <b-row>
+      <h1>Tuesday</h1>
+      <b-col v-for="tuesdayslot in tuesdayslots" v-bind:key="tuesdayslot" align-h="center">
+        <b-button close>{{ tuesdayslot }}</b-button>
+      </b-col>
+      </b-row>
+      <b-row>
+      <h1>Wednesday</h1>
+      <b-col v-for="wednesdayslot in wednesdayslots" v-bind:key="wednesdayslot" align-h="center">
+        <b-button close>{{ wednesdayslot }}</b-button>
+      </b-col>
+      </b-row>
+      <b-row>
+      <h1>Thursday</h1>
+      <b-col v-for="thursdayslot in thursdayslots" v-bind:key="thursdayslot" align-h="center">
+        <b-button close>{{ thursdayslot }}</b-button>
+      </b-col>
+      </b-row>
+      <b-row>
+      <h1>Friday</h1>
+      <b-col v-for="fridayslot in fridayslots" v-bind:key="fridayslot" align-h="center">
+        <b-button close>{{ fridayslot }}</b-button>
+      </b-col>
+      </b-row>
     </div>
     <div id="form-div">
       <BookingForm
@@ -36,15 +63,50 @@ export default {
    'dentistimo/dentists/dentist' (data) {
       var jsonData = JSON.parse(data)
       this.office = jsonData
-      console.log(jsonData)
+      this.mondayslots = this.getTimeSlots(jsonData[0].openinghours.monday)
+      this.tuesdayslots = this.getTimeSlots(jsonData[0].openinghours.tuesday)
+      this.wednesdayslots = this.getTimeSlots(jsonData[0].openinghours.wednesday)
+      this.thursdayslots = this.getTimeSlots(jsonData[0].openinghours.thursday)
+      this.fridayslots = this.getTimeSlots(jsonData[0].openinghours.friday)
     }
   },
   data() {
     return {
       offices: [],
-      office: ''
+      office: '',
+      mondayslots: '',
+      tuesdayslots: '',
+      wednesdayslots: '',
+      thursdayslots: '',
+      fridayslots: ''
+
     }
-}
+  },
+  methods: {
+    getTimeSlots(dailyhours){
+      var res= dailyhours.split("-"); 
+      let openingHour = res[0].split(":");
+      let closingHour = res[1].split(":");
+      var timeslots = [];
+      var i;
+
+      var finalopeningHour = openingHour[0];
+      var finalclosingHour = closingHour[0];
+
+      for(i = parseInt(finalopeningHour); i < parseInt(finalclosingHour); i++) {
+        var timeslotStart = i + ":00"
+        var timeslotEnda = i + ":30"
+        timeslots.push(timeslotStart + "-" + timeslotEnda)
+        timeslotStart = i + ":30"
+        timeslotEnda = i+1 + ":00"
+        timeslots.push(timeslotStart + "-" + timeslotEnda)
+      }
+      console.log(timeslots)
+      console.log('timeslots innan return:' + timeslots)
+      return timeslots;
+
+    }  
+  }
 }
 </script>
 
