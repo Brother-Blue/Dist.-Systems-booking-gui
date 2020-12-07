e0e31404480b0b71b29a5a75e3a291c0b7594d52
hugohe <hugohhempel@gmail.com>
2020-12-07 12:36:21 +0100

    add Calendar component and initial calendar

---
+++ client/src/components/Calendar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-calendar v-model="value" :min="min" :max="max" locale="en"></b-calendar>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      const now = new Date()
+      const today = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+      const minDate = new Date(today)
+      minDate.setMonth(minDate.getMonth() - 2)
+      minDate.setDate(15)
+      const maxDate = new Date(today)
+      maxDate.setMonth(maxDate.getMonth() + 2)
+      maxDate.setDate(15)
+
+      return {
+        value: '',
+        min: minDate,
+        max: maxDate
+      }
+    }
+  }
+</script>
