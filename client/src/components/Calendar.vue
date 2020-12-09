305910364a5ed08aae1931fdc737741bc62846fe
Hugo Hempel <Hugohhempel@gmail.com>
2020-12-09 09:07:02 +0100

    disable saturday and sunday from calendar

--- client/src/components/Calendar.vue
+++ client/src/components/Calendar.vue
@@ -1,7 +1,7 @@
 <template>
   <b-row no-gutters>
     <b-col cols="12">
-      <b-calendar class=calendar block v-model="value" locale="en-US"></b-calendar>
+      <b-calendar class=calendar block v-model="value" :date-disabled-fn="dateDisabled" locale="en-US"></b-calendar>
     </b-col>
     <p>{{value}}</p>
   </b-row>
@@ -15,6 +15,13 @@
       }
     },
     methods: {
+      dateDisabled(ymd, date) {
+        // Disable weekends (Sunday = `0`, Saturday = `6`) and
+        // disable days that fall on the 13th of the month
+        const weekday = date.getDay()
+        // Return `true` if the date should be disabled
+        return weekday === 0 || weekday === 6
+      }
     }
   }
 </script>
