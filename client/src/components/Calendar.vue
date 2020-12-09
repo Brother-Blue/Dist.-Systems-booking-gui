77d13e9dc4f440099895d59b3de6879fd3c4722c
Hugo Hempel <Hugohhempel@gmail.com>
2020-12-09 10:23:46 +0100

    added method that checks what specific day of the week was selected.

--- client/src/components/Calendar.vue
+++ client/src/components/Calendar.vue
@@ -1,7 +1,7 @@
 <template>
   <b-row no-gutters>
     <b-col cols="12">
-      <b-calendar class=calendar
+      <b-calendar class="calendar"
        block 
        start-weekday="1" 
        v-model="value" 
@@ -33,7 +33,12 @@
         value: '',
         day: '',
         min: minDate,
-        max: maxDate
+        max: maxDate,
+        monday: 1,
+        thuesday: 2,
+        wednesday: 3,
+        thursday: 4,
+        friday: 5
 
       }
     },
@@ -41,13 +46,33 @@
       dateDisabled(ymd, date) {
         // Disable weekends (Sunday = `0`, Saturday = `6`).
         const weekday = date.getDay()
-        console.log(weekday)
         // Return `true` if the date should be disabled
         return weekday === 0 || weekday === 6
       },
       showTimeslots(date) {
-        this.day = date.getDay()
-        console.log(this.day)
+        // getDay() returns a value representaing the day of the week, sunday=0, monday=1...saturday=6
+        console.log(date);
+        const daySelected = new Date(date).getDay()
+
+        //series of if statements checking what day was seleceted. this.weekday represents an int value equal to that day.
+        if(daySelected === this.monday) {
+          //code for the array of timeslots to be used in the calendar.
+        }
+        if(daySelected === this.thuesday) {
+          //code for the array of timeslots to be used in the calendar.
+        }
+        if(daySelected === this.wednesday) {
+          //code for the array of timeslots to be used in the calendar.
+        }
+        if(daySelected === this.thursday) {
+          //code for the array of timeslots to be used in the calendar.
+        }
+        if(daySelected === this.friday) {
+          //code for the array of timeslots to be used in the calendar.
+        }else {
+          //some sort of error logging.
+        }
+        console.log(daySelected);
       }
     }
   }
