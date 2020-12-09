b9c2ab96e4c2568b8b1b2b468f4d7c49953d83d4
Hugo Hempel <Hugohhempel@gmail.com>
2020-12-09 09:46:16 +0100

    restricted calendar to not show previous months, and only 2 months forward

--- client/src/components/Calendar.vue
+++ client/src/components/Calendar.vue
@@ -1,7 +1,17 @@
 <template>
   <b-row no-gutters>
     <b-col cols="12">
-      <b-calendar class=calendar block v-model="value" :date-disabled-fn="dateDisabled" locale="en-US"></b-calendar>
+      <b-calendar class=calendar
+       block 
+       start-weekday="1" 
+       v-model="value" 
+       :date-disabled-fn="dateDisabled" 
+       :min="min"
+       :max="max"
+       locale="en-US"
+       @selected="showTimeslots(value)" 
+       >
+       </b-calendar>
     </b-col>
     <p>{{value}}</p>
   </b-row>
@@ -10,16 +20,34 @@
 <script>
   export default {
     data() {
+    const date = new Date()
+    const today = new Date(date.getFullYear(), date.getMonth(), date.getDate())
+    const minDate = new Date(today)
+    const maxDate = new Date(today)
+    minDate.setMonth(today.getMonth())
+    minDate.setDate(today.getDate())
+    maxDate.setMonth(today.getMonth() + 2)
+    maxDate.setDate(today.getDate())
+
       return {
-        value: ''
+        value: '',
+        day: '',
+        min: minDate,
+        max: maxDate
+
       }
     },
     methods: {
       dateDisabled(ymd, date) {
         // Disable weekends (Sunday = `0`, Saturday = `6`).
         const weekday = date.getDay()
+        console.log(weekday)
         // Return `true` if the date should be disabled
         return weekday === 0 || weekday === 6
+      },
+      showTimeslots(date) {
+        this.day = date.getDay()
+        console.log(this.day)
       }
     }
   }
