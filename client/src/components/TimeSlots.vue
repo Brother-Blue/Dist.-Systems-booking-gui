712b9b11dc8aa8cf502c6ddd68cc61fa4b5bf437
jensenb <jensenb@student.chalmers.se>
2020-12-11 14:03:18 +0100

    Add backend data through subscriber into timeslots

--- client/src/components/TimeSlots.vue
+++ client/src/components/TimeSlots.vue
@@ -65,55 +65,40 @@ export default {
       'timeslotDay'
   ],
   mounted() {
-    this.$mqtt.publish('dentistimo/dentistoffice', JSON.stringify({'method': 'getOne','id': `${this.$route.params.id}`}))
-    this.$mqtt.subscribe('dentistimo/dentists/dentist')
+    this.$mqtt.publish('dentistimo/dentistoffice', JSON.stringify({'method': 'getAllTimeslots'}))
+    this.$mqtt.subscribe('dentistimo/dentists/offices/timeslots')
   },
   mqtt: {
-   'dentistimo/dentists/dentist' (data) {
+   'dentistimo/dentists/offices/timeslots' (data) {
       var jsonData = JSON.parse(data)
       this.office = jsonData
-      this.mondayslots = this.getTimeSlots(jsonData[0].openinghours.monday)
-      this.tuesdayslots = this.getTimeSlots(jsonData[0].openinghours.tuesday)
-      this.wednesdayslots = this.getTimeSlots(jsonData[0].openinghours.wednesday)
-      this.thursdayslots = this.getTimeSlots(jsonData[0].openinghours.thursday)
-      this.fridayslots = this.getTimeSlots(jsonData[0].openinghours.friday)
+      
+      for(var i = 0; i < this.office.length; i++){
+
+        if (this.office[i].id == `${this.$route.params.id}`){
+          this.mondayslots = this.office[i].timeslots.monday;
+          this.tuesdayslots = this.office[i].timeslots.tuesday;
+          this.wednesdayslots = this.office[i].timeslots.wednesday;
+          this.thursdayslots = this.office[i].timeslots.thursday;
+          this.fridayslots = this.office[i].timeslots.friday;
+        }  
+
+      }
     }
   },
   data() {
     return {
       offices: [],
       office: '',
-      mondayslots: '',
-      tuesdayslots: '',
-      wednesdayslots: '',
-      thursdayslots: '',
-      fridayslots: ''
+      mondayslots: [],
+      tuesdayslots: [],
+      wednesdayslots: [],
+      thursdayslots: [],
+      fridayslots: []
 
     }
   },
   methods: {
-    getTimeSlots(dailyhours){
-      var res= dailyhours.split("-"); 
-      let openingHour = res[0].split(":");
-      let closingHour = res[1].split(":");
-      var timeslots = [];
-      var i;
-
-      var finalopeningHour = openingHour[0];
-      var finalclosingHour = closingHour[0];
-
-      for(i = parseInt(finalopeningHour); i < parseInt(finalclosingHour); i++) {
-        var timeslotStart = i + ":00"
-        var timeslotEnda = i + ":30"
-        timeslots.push(timeslotStart + "-" + timeslotEnda)
-        timeslotStart = i + ":30"
-        timeslotEnda = i+1 + ":00"
-        timeslots.push(timeslotStart + "-" + timeslotEnda)
-      }
-      console.log(timeslots)
-      console.log('timeslots innan return:' + timeslots)
-      return timeslots;
-    }  
   }
 }
 </script>
